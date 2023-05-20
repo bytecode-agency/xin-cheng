a891a685bf460ea5633f1cf6a20777a21a9465e8
PRAKASH MANDAL <prakashmandal@PRAKASHs-MacBook-Pro.local>
2023-05-20 11:39:43 +0530

    code updated

--- resources/views/wealth/view.blade.php
+++ resources/views/wealth/view.blade.php
@@ -785,9 +785,7 @@
                                                 <label for="" class="form-label">Commencement Date</label>
                                                 <p>
                                                     @isset($wealth_mas->commencement_date)
-
-                                                    {{ date("d F Y", strtotime($wealth_mas->commencement_date)) }}
-
+                                                        {{ $wealth_mas->commencement_date }}
                                                     @else
                                                         -
                                                     @endisset
@@ -808,9 +806,7 @@
                                                     Deadline</label>
                                                 <p>
                                                     @isset($wealth_mas->annual_declaration_deadline)
-
-                                                    {{ date("d F Y", strtotime($wealth_mas->annual_declaration_deadline)) }}
-
+                                                        {{ $wealth_mas->annual_declaration_deadline }}
                                                     @else
                                                         -
                                                     @endisset
