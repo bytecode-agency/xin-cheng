f9796bc250880ee1229a89cf6d0adea778e89f94
Naresh <mandloinaresh@gmail.com>
2023-06-18 12:53:52 +0530

    rename phone num

--- public/js/wealth.js
+++ public/js/wealth.js
@@ -1187,7 +1187,7 @@ $(document).ready(function () {
                             <input type="integer" name="share[` + (shr_arr_id) + `][` + (sh_no + 1) + `][monthly_salary_wef]" id="fo_cpm2_month_wef_` + (shr_arr_id)+(sh_no + 1) + `" class="form-control datepicker" value="" placeholder="dd/mm/yy">
                         </div>
                         <div class="formAreahalf">
-                            <label for="fo_cpm2_phone_`+(shr_arr_id)+(sh_no + 1)+`" class="form-label">Phone Number two</label>
+                            <label for="fo_cpm2_phone_`+(shr_arr_id)+(sh_no + 1)+`" class="form-label">Phone Number</label>
                             <input type="text" name="share[` + (shr_arr_id) + `][` + (sh_no + 1) + `][phone]" id="fo_cpm2_phone_` + (shr_arr_id)+(sh_no + 1) + `" class="form-control"
                                 value="">
                         </div>
