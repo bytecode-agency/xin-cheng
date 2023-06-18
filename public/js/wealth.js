c1f8bcee96306980db6c26697eddc6ce5d4f22e9
Pankaj Kumar Yadav <vy8412@gmail.com>
2023-06-18 12:01:49 +0530

    WMF - Percentage speling corrected

--- public/js/wealth.js
+++ public/js/wealth.js
@@ -1509,7 +1509,7 @@ $(document).ready(function () {
                                 <h4>Shareholder #1</h4>
                             </div>
                             <div class="formAreahalf">
-                                <label for="nfo_equity" class="form-label">Equity Percentageee</label>
+                                <label for="nfo_equity" class="form-label">Equity Percentage</label>
                                     <div class="dollersec percentage_input"><span class="input">
                                     <input type="text" name="shrd[1][1][nfo_equity]" id="nfo_equity" class="form-control equity_shareholders" value=""></span>
                                     <span class="pecentage_end">%</span>
